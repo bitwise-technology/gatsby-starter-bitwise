fd06cf5374a2d89070f2c793617d2d7ebbb674e8
eduviictor <eduvictornobrega@gmail.com>
2020-04-22 10:11:31 -0300

    chore: add lint-staged and husky

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Bitwise Starter',
     description:
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    author: '@eduviictor',
   },
   plugins: [
     {
